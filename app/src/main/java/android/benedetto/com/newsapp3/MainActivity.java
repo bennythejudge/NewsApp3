ec573487456f3b78220f4d686d7b191936f8f659
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2018-06-04 23:23:24 +0200

    Add settings menu

--- app/src/main/java/android/benedetto/com/newsapp3/MainActivity.java
+++ app/src/main/java/android/benedetto/com/newsapp3/MainActivity.java
@@ -1,7 +1,10 @@
 package android.benedetto.com.newsapp3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,9 +17,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
-
+        int id = item.getItemId();
+        Log.d("The eagles", "id: " + id);
+        if (id == R.id.action_settings) {
+            Log.d("the eagles", "I am inside this fucking if");
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/android/benedetto/com/newsapp3/SettingsActivity.java
+++ app/src/main/java/android/benedetto/com/newsapp3/SettingsActivity.java
@@ -1,24 +1,85 @@
 package android.benedetto.com.newsapp3;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import java.util.List;
 
 public class SettingsActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        Log.d("newsapp3settings", "inside SettingsActivity");
+        setContentView(R.layout.settings_activity);
+    }
 
-        /* The above code is the typical pattern used to add a fragment to an activity so that
-         the fragment appears as the main content of the activity. You use:
-         getFragmentManager() if the class extends Activity and the fragment extends
-         PreferenceFragment.
-         getSupportFragmentManager() if the class extends AppCompatActivity and the fragment
-         extends PreferenceFragmentCompat.
-        */
-        getSupportFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
+    public static class NewsApp3PreferenceFragment
+            extends PreferenceFragment
+            implements Preference.OnPreferenceChangeListener {
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+
+            Log.d("THECHALET", "inside onCreate inside NewsApp3PreferenceFragment");
+
+            Preference searchString = findPreference(getString(R.string.settings_search_string_key));
+
+            Log.d("The Eagles", "Take it easy");
+            bindPreferenceSummaryToValue(searchString);
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+
+            Log.d("The eagles", "inside bindPreferenceSummaryToValue");
+
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+
+            Log.d("THECHALET", "inside onPreferenceChange preference: " + preference);
+
+            String stringValue = value.toString();
+
+            Log.d("THECHALET", "point 9 inside onPreferenceChange2 stringValue: " + stringValue);
+
+
+            if (preference instanceof ListPreference) {
+                Log.d("THECHALET", "inside onPreferenceChange inside if");
+
+                ListPreference listPreference = (ListPreference) preference;
+                int prefIndex = listPreference.findIndexOfValue(stringValue);
+                Log.d("THECHALET", "point 10 prefIndex : " + prefIndex);
+                if (prefIndex >= 0) {
+                    CharSequence[] labels = listPreference.getEntries();
+                    Log.d("THECHALET", "inside onPreferenceChange inside if about to setSummary");
+                    Log.d("THECHALET", "prefIndex: " + prefIndex);
+                    Log.d("THECHALET", "labels: " + labels);
+                    Log.d("THECHALET", "labels length: " + labels.length);
+
+                    preference.setSummary(labels[prefIndex]);
+                }
+            } else {
+                Log.d("THECHALET", "inside onPreferenceChange inside else");
+
+                preference.setSummary(stringValue);
+
+                Log.d("THECHALET", "inside onPreferenceChange after setSummray");
+
+            }
+            return true;
+        }
     }
 }
